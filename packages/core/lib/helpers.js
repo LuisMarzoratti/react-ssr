4738a2a7ce3ef053c702fe7163e25b550fc899bb
Luis Marzoratti <luis.marzora@gmail.mcom>
2021-05-16 16:55:48 -0300

    remove ssr env var

--- packages/core/lib/helpers.js
+++ packages/core/lib/helpers.js
@@ -1,8 +1,5 @@
 const isProd = () => {
   let env = process.env.NODE_ENV || 'development';
-  if (process.env.REACT_SSR_ENV) {
-    env = process.env.REACT_SSR_ENV;
-  }
   return env === 'production';
 };
 
--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -15,9 +15,6 @@ export const existsSync = (f: string): boolean => {
 
 export const isProd = () => {
   let env = process.env.NODE_ENV || 'development';
-  if (process.env.REACT_SSR_ENV) {
-    env = process.env.REACT_SSR_ENV;
-  }
   return env === 'production';
 };
 
